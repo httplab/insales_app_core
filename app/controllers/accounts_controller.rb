e21adf786e516b160e480bc8f97ab478a8615c9d
Alex Vasyutin <alex@httplab.ru>
2014-03-17 17:06:43 +0400

    Поправлен баг приводивший к невозможности сделать uninstall аккаунта

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,10 +2,10 @@ class AccountsController < ApplicationController
   skip_before_filter :authentication, :configure_api
 
   def install
-    logger.info '--- AccountsController#install'
-    logger.info "shop: #{params[:shop]}"
-    logger.info "token: #{params[:token]}"
-    logger.info "insales_id: #{params[:insales_id]}"
+    logger.info '[insales_app_core] AccountsController#install'
+    logger.info "[insales_app_core] shop: #{params[:shop]}"
+    logger.info "[insales_app_core] token: #{params[:token]}"
+    logger.info "[insales_app_core] insales_id: #{params[:insales_id]}"
 
     shop = InsalesApi::App.prepare_shop(params[:shop])
     password = InsalesApi::App.password_by_token(params[:token])
@@ -21,12 +21,12 @@ class AccountsController < ApplicationController
   end
 
   def uninstall
-    logger.info '--- AccountsController#uninstall'
-    logger.info "shop: #{params[:shop]}"
-    logger.info "token: #{params[:token]}"
+    logger.info '[insales_app_core] AccountsController#uninstall'
+    logger.info "[insales_app_core] shop: #{params[:shop]}"
+    logger.info "[insales_app_core] token: #{params[:token]}"
 
     shop = InsalesApi::App.prepare_shop(params[:shop])
-    password = InsalesApi::App.password_by_token(params[:token])
+    password = params[:token]
 
     Account.find_by!(insales_subdomain: shop, insales_password: password).destroy!
 
