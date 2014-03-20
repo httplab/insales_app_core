34cd736bafb5c16f2e4743eae7e8611eb890fb34
Alex Vasyutin <alex@httplab.ru>
2014-03-20 17:00:45 +0400

    Работа над багом приводящим к отвалу компиляции ассетов

--- lib/insales_app_core/controller_extensions/authorization.rb
+++ lib/insales_app_core/controller_extensions/authorization.rb
@@ -1,89 +1,91 @@
-module InsalesAppCore::ControllerExtensions
-  module Authorization
-    extend ActiveSupport::Concern
+module InsalesAppCore
+  module ControllerExtensions
+    module Authorization
+      extend ActiveSupport::Concern
 
-    included do
-      before_filter :store_after_sign_in_location, :authenticate, :configure_api
+      included do
+        before_filter :store_after_sign_in_location, :authenticate, :configure_api
 
-      helper_method :current_account, :current_insales_app, :logged_in?
-    end
-
-    def authenticate
-      logout if enter_from_different_shop?
-
-      # Если приложение Insales висит в API и авторизовано, просто ищем нужный акконут.
-      if current_insales_app && current_insales_app.authorized?
-        return if self.current_account = Account.find_by_insales_subdomain(current_insales_app.shop)
+        helper_method :current_account, :current_insales_app, :logged_in?
       end
 
-      # В противном случае предполагаем, что нужная информация есть в params
-      # и пробуем авторизовать клиента.
-      if authorize_by_params!
-        redirect_to current_insales_app.authorization_url
-      else
-        redirect_to new_session_path
-      end
-    end
+      def authenticate
+        logout if enter_from_different_shop?
 
-    def store_after_sign_in_location
-      unless logged_in?
-        session[:after_sign_in_location] = request.fullpath
-      end
-    end
+        # Если приложение Insales висит в API и авторизовано, просто ищем нужный акконут.
+        if current_insales_app && current_insales_app.authorized?
+          return if self.current_account = Account.find_by_insales_subdomain(current_insales_app.shop)
+        end
 
-    def configure_api
-      current_insales_app.configure_api if current_insales_app
-    end
-
-    def logout
-      reset_session
-    end
-
-    def authorize_by_params!
-      self.current_account = if params[:insales_id]
-        Account.find_by_insales_id(params[:insales_id])
-      else
-        shop_wo_http = (params[:shop] || "").gsub('http://', '')
-        Account.find_by_insales_subdomain(InsalesApi::App.prepare_shop(shop_wo_http))
+        # В противном случае предполагаем, что нужная информация есть в params
+        # и пробуем авторизовать клиента.
+        if authorize_by_params!
+          redirect_to current_insales_app.authorization_url
+        else
+          redirect_to new_session_path
+        end
       end
 
-      return nil unless @current_account
-      self.current_insales_app = InsalesApi::App.new(current_account.insales_subdomain, current_account.insales_password)
-    end
+      def store_after_sign_in_location
+        unless logged_in?
+          session[:after_sign_in_location] = request.fullpath
+        end
+      end
 
-    def enter_from_different_shop?
-      return false unless params[:shop]
+      def configure_api
+        current_insales_app.configure_api if current_insales_app
+      end
 
-      params_shop = InsalesApi::App.prepare_shop(params[:shop])
-      current_insales_app && params_shop != current_insales_app.shop
-    end
+      def logout
+        reset_session
+      end
 
-    def logged_in?
-      current_insales_app.present? && current_insales_app.authorized? && current_account.present?
-    end
+      def authorize_by_params!
+        self.current_account = if params[:insales_id]
+          Account.find_by_insales_id(params[:insales_id])
+        else
+          shop_wo_http = (params[:shop] || "").gsub('http://', '')
+          Account.find_by_insales_subdomain(InsalesApi::App.prepare_shop(shop_wo_http))
+        end
 
-    def current_account
-      @current_account
-    end
+        return nil unless @current_account
+        self.current_insales_app = InsalesApi::App.new(current_account.insales_subdomain, current_account.insales_password)
+      end
 
-    def current_account=(acc)
-      @current_account = acc
-    end
+      def enter_from_different_shop?
+        return false unless params[:shop]
 
-    def current_insales_app
-      session[:current_insales_app]
-    end
+        params_shop = InsalesApi::App.prepare_shop(params[:shop])
+        current_insales_app && params_shop != current_insales_app.shop
+      end
 
-    def current_insales_app=(app)
-      session[:current_insales_app] = app
-    end
+      def logged_in?
+        current_insales_app.present? && current_insales_app.authorized? && current_account.present?
+      end
 
-    def clear_after_sign_in_location
-      session[:after_sign_in_location] = nil
-    end
+      def current_account
+        @current_account
+      end
 
-    def after_sign_in_location
-      session[:after_sign_in_location]
+      def current_account=(acc)
+        @current_account = acc
+      end
+
+      def current_insales_app
+        session[:current_insales_app]
+      end
+
+      def current_insales_app=(app)
+        session[:current_insales_app] = app
+      end
+
+      def clear_after_sign_in_location
+        session[:after_sign_in_location] = nil
+      end
+
+      def after_sign_in_location
+        session[:after_sign_in_location]
+      end
     end
   end
 end
