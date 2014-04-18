01bad61f231727f7fd63d4b36d8d22cf7a1201bb
Yury Kotov <bairkan@gmail.com>
2014-04-18 16:19:09 +0400

    Убрал концерн для котроллера AccountSettings

--- app/controllers/account_settings_controller.rb
+++ app/controllers/account_settings_controller.rb
@@ -1,7 +1,26 @@
 class AccountSettingsController < ApplicationController
-  include AccountSettingsControllerBase
-
   respond_to :html
   responders :flash
   before_filter :fetch_account_settings
+
+  def edit
+    respond_with @account_settings
+  end
+
+  def update
+    @account_settings.update_attributes(fetch_params)
+    respond_with @account_settings, location: edit_account_settings_path
+  end
+
+  def fetch_account_settings
+    @account_settings = current_account.settings
+  end
+
+  def fetch_params
+    params.require(:account_settings).permit(*permitted_params)
+  end
+
+  def permitted_params
+    []
+  end
 end
