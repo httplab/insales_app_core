41d6f9ed5677ca0affa08525d4e912123c464b37
sk <sk@httplab.ru>
2014-07-09 18:30:31 +0400

    Добавлена локализация, изменен домен магазина в factory аккаунта

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :account do
-    insales_id {rand(20000..30000) }
-    insales_password {Faker::Internet.password(32) }
-    insales_subdomain {Faker::Internet.domain_name}
+    insales_id { rand(20000..30000) }
+    insales_password { Faker::Internet.password(32) }
+    insales_subdomain { "#{Faker::Internet.domain_word}/myinsales.ru }
   end
 end
