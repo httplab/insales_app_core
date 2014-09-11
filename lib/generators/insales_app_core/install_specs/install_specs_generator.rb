3b2b9e34b690901f130db4471704c3ab9d4c2f96
Alex Vasyutin <alex@httplab.ru>
2014-09-11 20:53:04 +0400

    card 964 в случае rspec3 подключение spec_helpers заменяется на rails_helpers

--- lib/generators/insales_app_core/install_specs/install_specs_generator.rb
+++ lib/generators/insales_app_core/install_specs/install_specs_generator.rb
@@ -11,6 +11,14 @@ class InsalesAppCore::InstallSpecsGenerator < Rails::Generators::Base
     directory 'models', 'spec/models'
   end
 
+  def replace_spec_helper
+    return unless ARGV.index('rspec3')
+
+    Dir.glob(Rails.root.join('spec','**/*_spec.rb')).each do |filename|
+      gsub_file filename, 'spec_helper', 'rails_helper'
+    end
+  end
+
   def message
     puts "\n>> После установки не забудьте подключить InsalesAppCore::TestHelpers::Controller".green
     puts '>> rails_helper.rb'.green
