67c19b2c22fd1be2e57d8ddd3874f2b3a6d881c4
Nu-hin <nuinuhin@gmail.com>
2014-11-07 15:44:32 +0300

    Опечатка

--- db/migrate/20141107123335_rename_fields_in_product_characteristics.rb
+++ db/migrate/20141107123335_rename_fields_in_product_characteristics.rb
@@ -1,6 +1,6 @@
 class RenameFieldsInProductCharacteristics < ActiveRecord::Migration
   def change
     rename_column :product_characteristics, :product_id, :insales_product_id
-    rename_column :product_characteristics, :characterisic_id, :insales_characteristic_id
+    rename_column :product_characteristics, :characteristic_id, :insales_characteristic_id
   end
 end
