499aa320b3a74c7102f2cc89fecfa9625049474c
Nu-hin <nuinuhin@gmail.com>
2015-03-25 16:52:58 +0300

    Поправлена инициализация сихронизатора в FirstSyncWorker

--- app/workers/insales_app_core/synchronization/first_sync_worker.rb
+++ app/workers/insales_app_core/synchronization/first_sync_worker.rb
@@ -7,7 +7,7 @@ module InsalesAppCore::Synchronization
     def perform(account_id)
       account = Account.find(account_id)
       account.configure_api
-      synchronizer = Synchronizer.new(account_id, InsalesAppCore.config.sync_options)
+      synchronizer = Synchronizer.new(account_id, sync: InsalesAppCore.config.sync_options)
       observers = InsalesAppCore.config.sync_observers_classes
       synchronizer.add_observer(InsalesAppCore::Synchronization::Observers::Logger.new)
       observers.each { |clazz| synchronizer.add_observer(clazz.new) }
